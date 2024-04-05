6270ce48baf34a141bb85e2539b4d6daaeb100e7
2302728 <2302728@salas.aulas>
2024-04-05 19:43:10 -0300

    Implementação e conversao de dados da API

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,32 +1,77 @@
 const url = 'https://api-go-wash-efc9c9582687.herokuapp.com/api/user';
 
-async function cadastroUsuario(){   
-    var name = document.getElementById('name');     
-    let resposta = await fetch(url,{
-        method:"POST",
-        body:JSON.stringify(
-            {
-                "name":name.value,
-                "email":"carlos@gmail.com",
-                "user_type_id":1,
-                "password": "123456",
-                "cpf_cnpj": "62418247406",
-                "terms": 1,
-                "birthday":"2000-10-12"    
-            }
-        ),
-        headers:{
-            'Content-Type': 'application/json'
-        }        
-    });
+async function cadastroUsuario() {
+  var name = document.getElementById('nome_completo');
+  var email = document.getElementById('email');
+  var cpf_cnpj = document.getElementById("cpf_cnpj");
+  var password = document.getElementById("password");
+  var password2 = document.getElementById("confirmPassword");
+  var birthday = document.getElementById("data_nascimento");
 
-    let data = await resposta.json();
-    
-    
-    if(data.data.statusCode != 200){
-        alert(data.data.errors?.cpf_cnpj[0]);
-        return;
+ /*if (!name.value || !email.value || !cpf_cnpj.value || !password.value || !birthday.value) {
+    alert('Por favor, preencha todos os campos obrigatórios.');
+    return;
+ }*/
+
+  var hoje = new Date();
+  var aniversario = new Date(birthday.value);
+  var idade = hoje.getFullYear() - aniversario.getFullYear();
+
+  if (password.value !== confirmPassword.value) {
+    alert("As senhas não coincidem");
+    return;
+  }
+  if (
+    hoje.getMonth() < aniversario.getMonth() ||
+    (hoje.getMonth() === aniversario.getMonth() &&
+      hoje.getDate() < aniversario.getDate())
+  ) {
+    idade--;
+  }
+
+  if (idade < 18) {
+    alert("Você deve ter 18 anos para continuar");
+    return;
+  }
+  
+
+  let respostaApi = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      "name": name.value,
+      "email": email.value,
+      "user_type_id": 1,
+      "password": password.value,
+      "cpf_cnpj": cpf_cnpj.value,
+      "terms": 1,
+      "birthday": birthday.value
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
-    alert("Cadastro feito com sucesso");
-    window.location.href = "login.html";
+  });
+
+  let nova_resposta = await respostaApi.json();
+  if (nova_resposta.data.statusCode === 422) {
+    if (nova_resposta.data.errors.cpf_cnpj) {
+      alert("Cpf/cnpj invalido, insira um valido ou não se tornará um marromba");
+    }
+    if (nova_resposta.data.errors.email) {
+      alert("email invalido, insira um valido ou não se tornará um marromba");
+    }
+    if (nova_resposta.data.errors.password) {
+      alert("A senha inserida, não atende aos requisitos");
+    }
+    if (nova_resposta.data.errors.aniversario) {
+      alert("data invalida, insira uma valida ou não se tornará um marromba");
+    }
+    if (nova_resposta.data.errors.name) {
+      alert("Nome invalido, insira um valido ou não se tornará um marromba");
+    }
+    
+  }
+  else{
+  alert("cadastro feito com sucesso")
+  window.open("../view/login.html")
+  }
 }
